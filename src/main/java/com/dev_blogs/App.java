324f20d353b021e140b7bdfaf16a07d21e486c63
zheka <zheka0783@yandex.ru>
2016-01-25 20:19:40 +0200

    Create project

---
+++ src/main/java/com/dev_blogs/App.java
@@ -0,0 +1,25 @@
+package com.dev_blogs;
+
+import org.apache.log4j.Logger;
+
+public class App {
+    final static Logger logger = Logger.getLogger(App.class);
+ 
+    public static void main(String[] args) {
+        App app = new App();
+        app.test();
+    }
+ 
+    public void test() {
+        logger.debug("debug"); // all
+        logger.info("info"); // except debug
+        logger.warn("warn"); // except debug and info
+        logger.error("error"); // except debug, info and warn
+        logger.fatal("fatal"); // only fatal
+        try {
+            throw new Exception("test exception");
+        } catch (Exception e) {
+            logger.error("exception", e);
+        }
+    }
+}
