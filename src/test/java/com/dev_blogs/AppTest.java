be247c470d703d7b6b303e9c7838b395612d7f35
zheka <zheka0783@yandex.ru>
2016-02-07 16:45:19 +0200

    changed tests

--- src/test/java/com/dev_blogs/AppTest.java
+++ src/test/java/com/dev_blogs/AppTest.java
@@ -2,11 +2,14 @@ package com.dev_blogs;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class AppTest {
 	
 	@Test
 	public void testApp() {
 		App app = new App();
 		app.logMessages();
+		Assert.assertEquals("test", "test1");
 	}
 }
